692b1968202dd6346951de34eb0de2b964b1d915
timothy <tk45quji@studserv.uni-leipzig.de>
2023-04-18 16:42:02 +0200

    Working Creating xml and jsonl

--- Code/Data.py
+++ Code/Data.py
@@ -1,5 +1,7 @@
 import json
 import os
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
 
 #Zählt die Einträge einer JSONL Datei
 def countEntriesJSONL(input_File):
@@ -32,24 +34,68 @@ def getEntriesToJSONL(input_File, output_File):
             output_file.write(json.dumps(filtered_data) + "\n")
 
 
-def get_5Elements(file_path):
-    # Öffne die Datei und lese sie zeilenweise ein
-    with open(file_path, "r") as f:
-        # Lese die ersten fünf Zeilen
-        for i, line in enumerate(f):
-            if i >= 5:
-                break
-            # Lade das JSON-Objekt aus der Zeile
-            obj = json.loads(line)
-            # Gib das Objekt aus
-            for key, value in obj.items():
-                print(key, ":", value, ",")
+def setXML(file_path, file_name):
+    if checkForXML(file_path, file_name) == False:
+        try:
+            root = ET.Element("topics")
+            tree = ET.ElementTree(root)
+            tree.write(file_path)
+            print(f"Die Datei {file_name} wurde erstellt.")
+        except Exception as e:
+            print(f"Fehler beim Erstellen der Datei {file_name}: {e}")
+    else:
+        print("Die Datei existiert bereits.")
+        overwrite = input('Soll die Datei überschrieben werden? ("Ja" oder "Nein"): ')
+        while overwrite not in ["Ja", "Nein"]:
+            overwrite = input('Falsche Eingabe! Soll die Datei überschrieben werden? ("Ja" oder "Nein"): ')
+
+        if overwrite == "Ja":
+            root = ET.Element("topics")
+            tree = ET.ElementTree(root)
+            tree.write(file_path)
+            print(f"Die Datei {file_name} wurde überschrieben.")
+        else:
+            tree = ET.parse(file_path)
+            root = tree.getroot()
+            print(f"Es befinden sich {len(root)} Topics in der Datei.")
+
+    topics = -1
+    confirmation = "Nein"
+    while (confirmation != "Ja"):
+        topics = input("Anzahl der Topics:")
+        print(f"Die Anzahl der Topics soll {topics} betragen.")
+        confirmation = input("Ist dies korrekt? (Ja/Nein):")
+
+    for i in range(int(topics)):
+        new_topic = ET.SubElement(root, "topic", number="")
+        new_title = ET.SubElement(new_topic, "title")
+        new_title.text = "  "
+        new_description = ET.SubElement(new_topic, "description")
+        new_description.text = "  "
+        new_narrative = ET.SubElement(new_topic, "narrative")
+        new_narrative.text = "  "
+
+    ET.indent(tree, '  ') #Richtige Einrückung für die Datei
+    tree.write(file_path) #Schreibt in die Datei
+
+"""
+Prüft, ob eine XML-Datei mit dem gegebenen Namen im gegebenen Verzeichnis vorhanden ist.
+Wenn die Datei nicht vorhanden ist, wird eine neue Datei mit dem gegebenen Namen im gegebenen Verzeichnis erstellt.
+"""
+def checkForXML(file_path, file_name):
+    if os.path.isfile(file_path):
+        print(f"Die Datei {file_name} existiert bereits.")
+        return True
+    else:
+        print(f"Die Datei {file_name} existiert nicht.")
+        return False
 
 
 #Überprüft, ob eine Datei im gegeben Pfad enthalten ist und erstellt ggf. eine
 def checkForFile(File_Path, File_Name):
     # Überprüfe, ob die JSONL-Datei bereits vorhanden ist
     if os.path.isfile(os.path.join(File_Path)):
+
         print("Die Datei existiert bereits.\n")
     else:
         # Öffne die Ausgabe-JSONL-Datei zum Schreiben
