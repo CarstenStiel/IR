a440ac8cb0e23ff9159cb07bc899a4e57e3f9b66
CarstenStiel <timothy.kriewald@gmail.com>
2023-04-24 16:29:26 +0200

    Added Jupiter Notebook with testlines of Code and created Dir for new Files

--- Code/main.py
+++ Code/main.py
@@ -5,12 +5,10 @@ xmlFileName = "topics.xml"
 inputFileName = "ir-anthology-07-11-2021-ss23.jsonl"
 outputFileName = "ir-anthology-final.jsonl"
 inputFilePath = Data.getDirectory() + inputFileName
-outputFilePath = Data.getDirectory() + outputFileName
-xmlFilePath = Data.getDirectory() + xmlFileName
+outputFilePath = Data.getDirectory() + "CreatedData\\" + outputFileName
+xmlFilePath = Data.getDirectory() + "CreatedData\\" + xmlFileName
 
 if __name__ == '__main__':
-
-    print(outputFilePath)
 
     Data.getEntriesToJSONL(inputFilePath, outputFilePath, outputFileName)
     print(f"Anzahl Entitäten in {outputFileName}: ", Data.countEntriesJSONL(outputFilePath), "\n")
