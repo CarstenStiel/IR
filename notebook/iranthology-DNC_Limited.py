93ac060aade2a60913c0cfff85797d1498b6e376
timothy <tk45quji@studserv.uni-leipzig.de>
2023-04-27 19:32:32 +0200

    Updated readme and refactoring

--- notebook/iranthology-DNC_Limited.py
+++ notebook/iranthology-DNC_Limited.py
@@ -6,7 +6,7 @@ from typing import NamedTuple, Dict
 from ir_datasets.util.download import RequestsDownload
 from ir_datasets.datasets.base import Dataset
 
-DATASET_URL = 'https://raw.githubusercontent.com/CarstenStiel/IR/main/notebook/CreatedData/'
+DATASET_URL = 'https://raw.githubusercontent.com/CarstenStiel/IR/main/notebook/data/'
 
 
 class DNCDocument(NamedTuple):
@@ -19,5 +19,5 @@ class DNCDocument(NamedTuple):
 
 ir_datasets.registry.register('iranthology-DNC_Limited', Dataset(
     JsonlDocs(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'ir-anthology-final.jsonl')], expected_md5='6fd25525cb321f6517c934f8ff8c96fb'), doc_cls=DNCDocument, lang='en'),
-    TrecXmlQueries(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'topics.xml')], expected_md5='0916773cf11a1e3d7580268086f52cf4'), lang='en')
+    TrecXmlQueries(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'topics.xml')], expected_md5='3cf876258c8db1c003d18804b4bce299'), lang='en')
 ))
