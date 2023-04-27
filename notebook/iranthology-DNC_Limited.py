c7df4badc41a88c54434ba22e40eb4e3afa9ad54
timothy <tk45quji@studserv.uni-leipzig.de>
2023-04-27 20:11:26 +0200

    Updated topics.xml

--- notebook/iranthology-DNC_Limited.py
+++ notebook/iranthology-DNC_Limited.py
@@ -18,6 +18,6 @@ class DNCDocument(NamedTuple):
 
 
 ir_datasets.registry.register('iranthology-DNC_Limited', Dataset(
-    JsonlDocs(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'ir-anthology-final.jsonl')], expected_md5='81aada046fedd8915aee2d5ef2f6aae3'), doc_cls=DNCDocument, lang='en'),
-    TrecXmlQueries(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'topics.xml')], expected_md5='3cf876258c8db1c003d18804b4bce299'), lang='en')
+    JsonlDocs(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'ir-anthology-final.jsonl')], expected_md5='c434caa3b4a2a37d6685c3b6c2c4b012'), doc_cls=DNCDocument, lang='en'),
+    TrecXmlQueries(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'topics.xml')], expected_md5='a06173bf3544ff66afcb3f513d30210f'), lang='en')
 ))
