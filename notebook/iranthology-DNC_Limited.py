24c92102d14d32c7ce03b39d1859fa38c7d399af
CarstenStiel <timothy.kriewald@gmail.com>
2023-04-28 13:25:48 +0200

    Updated m5 hash

--- notebook/iranthology-DNC_Limited.py
+++ notebook/iranthology-DNC_Limited.py
@@ -19,5 +19,5 @@ class DNCDocument(NamedTuple):
 
 ir_datasets.registry.register('iranthology-DNC_Limited', Dataset(
     JsonlDocs(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'ir-anthology-final.jsonl')], expected_md5='c434caa3b4a2a37d6685c3b6c2c4b012'), doc_cls=DNCDocument, lang='en'),
-    TrecXmlQueries(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'topics.xml')], expected_md5='a06173bf3544ff66afcb3f513d30210f'), lang='en')
+    TrecXmlQueries(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'topics.xml')], expected_md5='86a5eaade1b550cff81078311e475e24'), lang='en')
 ))
