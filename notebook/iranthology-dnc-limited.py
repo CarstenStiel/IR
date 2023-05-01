9fc2e99ff22862ff35a4dc6b5ac7826af527c216
CarstenStiel <timothy.kriewald@gmail.com>
2023-05-01 23:20:32 +0200

    Final update

--- notebook/iranthology-dnc-limited.py
+++ notebook/iranthology-dnc-limited.py
@@ -1,10 +1,7 @@
 import ir_datasets
 from ir_datasets.formats import JsonlDocs, TrecXmlQueries, TrecQrels
 from typing import NamedTuple
-from ir_datasets.util.download import RequestsDownload
 from ir_datasets.datasets.base import Dataset
-
-DATASET_URL = 'https://raw.githubusercontent.com/CarstenStiel/IR/main/notebook/data/'
 
 
 class DNCDocument(NamedTuple):
@@ -16,6 +13,6 @@ class DNCDocument(NamedTuple):
 
 
 ir_datasets.registry.register('iranthology-dnc-limited', Dataset(
-    JsonlDocs(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'dnc-limited-documents.jsonl')], expected_md5='c434caa3b4a2a37d6685c3b6c2c4b012'), doc_cls=DNCDocument, lang='en'),
-    TrecXmlQueries(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'dnc-limited-topics.xml')], expected_md5='93458db12a666b8b1166d39446d0d759'), lang='en')
+    JsonlDocs(ir_datasets.util.PackageDataFile(path='datasets_in_progress/dnc-limited-documents.jsonl'), doc_cls=DNCDocument, lang='en'),
+    TrecXmlQueries(ir_datasets.util.PackageDataFile(path='datasets_in_progress/dnc-limited-topics.xml'), lang='en')
 ))
